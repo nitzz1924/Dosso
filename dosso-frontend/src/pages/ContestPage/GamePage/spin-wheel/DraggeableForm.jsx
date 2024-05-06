969a4f77bf8ff95c641f5087a5485a9eae7773b7
Anshul Kumar <anshulkm17@gmail.com>
2024-05-06 18:02:31 +0530

    updated spin wheel counter

--- dosso-frontend/src/pages/ContestPage/GamePage/spin-wheel/DraggeableForm.jsx
+++ dosso-frontend/src/pages/ContestPage/GamePage/spin-wheel/DraggeableForm.jsx
@@ -6,43 +6,53 @@ const FormularioTexto = () => {
   const inputList = [
     {
       id: uuidv4(),
-      text: "1"
+      text: "?",
+      value: "1456"
     },
     {
       id: uuidv4(),
-      text: "2"
+      text: "?",
+      value: "2345"
     },
     {
       id: uuidv4(),
-      text: "3"
+      text: "?",
+      value: "3456"
     },
     {
       id: uuidv4(),
-      text: "4"
+      text: "?",
+      value: "4567"
     },
     {
       id: uuidv4(),
-      text: "5"
+      text: "?",
+      value: "5678"
     },
     {
       id: uuidv4(),
-      text: "6"
+      text: "?",
+      value: "6789"
     },
     {
       id: uuidv4(),
-      text: "7"
+      text: "?",
+      value: "7894"
     },
     {
       id: uuidv4(),
-      text: "8"
+      text: "?",
+      value: "8974"
     },
     {
       id: uuidv4(),
-      text: "9"
+      text: "?",
+      value: "9874"
     },
     {
       id: uuidv4(),
-      text: "10"
+      text: "?",
+      value: "1111"
     },
 
   ];
