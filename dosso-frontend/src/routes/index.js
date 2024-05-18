1f04513fb18a49611b03074d3ebceef47b5f1c9b
unknown <nitesh.yuvmedia@gmail.com>
2024-05-18 13:58:40 +0530

    Overlay Problem on Withdrawl is solved

--- dosso-frontend/src/routes/index.js
+++ dosso-frontend/src/routes/index.js
@@ -56,7 +56,7 @@ import MyFund from "pages/Authentication/Wallet/MyFund";
 import Refer from "pages/Referrals/refer";
 import AddFund from "pages/Authentication/Wallet/AddFund";
 import LeaderBoard from "pages/ContestPage/GamePage/LeaderBoard";
-import Playerkyc from "pages/Authentication/kyc/Playerkyc";
+import Playerkyc from "pages/Authentication/kyc/playerkyc";
 
 const authProtectedRoutes = [
   { path: "/", component: <Contests /> },
